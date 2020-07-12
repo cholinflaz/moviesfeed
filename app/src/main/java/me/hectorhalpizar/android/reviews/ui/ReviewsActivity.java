5b0447ca34fbe3d9407b017e572b4bd03ec0be77
Hector Alpizar <hector.alpizar.c@gmail.com>
2020-07-11 22:38:04 -0700

    View design. Design Support libs and other libraries added. Recycler view added.
    Minor changes
    .idea dictionary folder added to .gitignore

---
+++ app/src/main/java/me/hectorhalpizar/android/reviews/ui/ReviewsActivity.java
@@ -0,0 +1,16 @@
+package me.hectorhalpizar.android.reviews.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.hectorhalpizar.android.reviews.R;
+
+public class ReviewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+    }
+}
